c709b99b5783c707ad49ac8512e751d14acc1907
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-13 09:03:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -32,6 +32,6 @@ end
 # It will take a Japanese emoticon and returns its meaning in English.
 # This method will also rely on load_library to first load the YAML file.
 def get_english_meaning(file_path, emoticon)
-  meaning = load_library(file_path)['get_meaning'][emoticon]  #sad
+  meaning = load_library(file_path)["get_meaning"][emoticon]  #sad
   meaning ? meaning : "Sorry, that emoticon was not found"
 end
