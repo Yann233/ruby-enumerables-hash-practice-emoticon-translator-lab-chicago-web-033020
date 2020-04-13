8c62d49c82eb42c190698f8702ed1bc69f05037d
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-13 08:37:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,7 @@ def load_library(file_path)
   binding.pry
   #the keys inside the 'get_emoticon' hash are the English emoticons
   #the emoticon keys inside the 'get_emoticon' hash point to their Japanese equivalents
-  result["get_emoticon"][emoticons[0]] = emoticons[1]
+
   end
   result
 end
